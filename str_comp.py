4d6f92182c3ecc040c9cdc8daf659edd87b0f75e
Roman Antonyuk <roman2062010@yandex.ru>
2020-11-29 19:33:26 +0500

    Задание №2: условный оператор if

---
+++ str_comp.py
@@ -0,0 +1,18 @@
+
+def strings (str_1, str_2):
+    if type(str_1) != str:
+        return 0
+    if type(str_2) != str:
+        return 0
+    if str_1 == str_2:
+        return 1
+    if str_1 > str_2:
+        return 2
+    if str_1 < str_2:
+        return 3
+
+
+str_1 = str(input("Введите информацию в строку №1: "))
+str_2 = str(input("Введите информацию в строку №2: "))
+
+print(strings(str_1, str_2))
